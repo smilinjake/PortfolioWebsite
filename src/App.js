30875543828bc442a8c9fb783f474899ff5c4867
Jacob Ward <smilinjake@gmail.com>
2022-12-16 19:06:37 +0100

    deleting unused variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Home from "./Components/Home.js";
 import InfoSection from "./Components/InfoSection.js";
 import ContactMe from "./Components/ContactMe.js";

1d513d6060a4cd6a980a3caf9f085fd53d41407e
Jacob Ward <smilinjake@gmail.com>
2023-11-02 16:40:00 -0500

    can not move pawns with no rules at all

--- src/Components/ChessPuzzle.js
+++ src/Components/ChessPuzzle.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ColorAssigner } from "./HelperFunctions/ChessHelpers.js";
 import "../App.css";
 
@@ -23,16 +23,56 @@ const ChessPuzzle = () => {
     }
   };
   makeBoard();
+  const [hookBoard, setHookBoard] = useState(board);
   // console.log(board);
-
+  const [selected, setSelected] = useState({
+    inHand: false,
+    name: "empty",
+    id: 0,
+  });
+  // NOW SOME PIECE MOVING LOGIC
+  const PieceMover = (arr, place) => {
+    if (selected.inHand === false) {
+      // selected.inHand = true;
+      // selected.name = place.piece;
+      setSelected({ inHand: true, name: place.piece, id: place.id - 1 });
+    }
+    // we take in an array that shows the current state of the board
+    // onClick we want to rearrange that array
+    // lets make Pawn Logic
+    let destination = place.id - 1;
+    if (selected.name === "p") {
+      console.log("just clicked p", place);
+      arr[destination].piece = selected.name;
+      arr[selected.id].piece = "e";
+      setHookBoard(arr);
+      setSelected({
+        inHand: false,
+        name: "empty",
+      });
+    }
+  };
+  // console.log(selected);
+  // console.log(board[53]);
+  // console.log(board[45]);
+  useEffect(() => {}, [hookBoard, selected]);
   return (
     <>
       <div className="ChessSection">
         <div className="BoardContainer">
-          {board.map((piece) => {
+          {hookBoard.map((piece, i) => {
             return (
               <div key={piece.id} id={piece.id} className={piece.className}>
-                {piece.piece !== "e" ? piece.piece : null}
+                <p
+                  onClick={(square) => {
+                    PieceMover(hookBoard, piece);
+                    console.log("piece", piece);
+                  }}
+                  className="FillerPiece"
+                >
+                  {piece.piece !== "e" ? piece.piece : "-"}
+                  {/* {i} */}
+                </p>
               </div>
             );
           })}
